59db056352a1e0abf2c9432208ac84fface48a43
Mohamad Firzon Ainur Roziqin <firzonainur@gmail.com>
2025-02-22 14:04:30 +0700

    first commit

--- content/_case1.php
+++ content/_case1.php
@@ -327,6 +327,24 @@
                                     </div>
                                 </div>
 
+                                 <div class="col-12">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            
+                                            <h5 class="header-title mb-4">Reasoning</h5>
+                                            <div id="yearly-sale-chart" class="apex-charts"></div>
+                                            <h3>
+                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                            </h3>
+                                        </div>
+                                    </div>
+                                </div>
+
                                 
                                 
 
